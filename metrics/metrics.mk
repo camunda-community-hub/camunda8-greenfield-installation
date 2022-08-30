11016e68eb55820ef5a8e709b4a54b07abf8484d
Falko Menge <falko.menge@camunda.com>
2022-08-30 14:42:04 +0200

    Restore metrics for GCP

--- metrics/metrics.mk
+++ metrics/metrics.mk
@@ -3,15 +3,15 @@ metrics:
 	helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 	helm repo add stable https://charts.helm.sh/stable
 	helm repo update prometheus-community stable
-	kubectl apply -f ./grafana-secret.yml -n default
-	helm install metrics prometheus-community/kube-prometheus-stack --wait --atomic -f ./prometheus-operator-values.yml --set prometheusOperator.tlsProxy.enabled=false --namespace default
-	kubectl apply -f ./grafana-load-balancer.yml -n default
+	kubectl apply -f ../metrics/grafana-secret.yml -n default
+	helm install metrics prometheus-community/kube-prometheus-stack --wait --atomic -f ../metrics/prometheus-operator-values.yml --set prometheusOperator.tlsProxy.enabled=false --namespace default
+	kubectl apply -f ../metrics/grafana-load-balancer.yml -n default
 
 .PHONY: clean-metrics
 clean-metrics:
-	-kubectl delete -f ./grafana-load-balancer.yml -n default
+	-kubectl delete -f ../metrics/grafana-load-balancer.yml -n default
 	-helm uninstall metrics --namespace default
-	-kubectl delete -f ./grafana-secret.yml -n default
+	-kubectl delete -f ../metrics/grafana-secret.yml -n default
 #	-kubectl delete -f $(include-dir)/ssd-storageclass.yaml -n default
 	-kubectl delete pvc -l app.kubernetes.io/name=prometheus -n default
 	-kubectl delete pvc -l app.kubernetes.io/name=grafana -n default
